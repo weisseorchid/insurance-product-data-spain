7f3727754c61179fd827bbf59d240597daac2f56
Ignacio Masdeu <weisse.orchid@gmail.com>
2026-01-26 22:32:10 +0100

    Remove obsolete insurance companies and distributors data files along with the initialization script. This cleanup enhances project maintainability by eliminating unused resources.

--- insurance_product_data_spain/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bundled insurance data files.
-
-This folder contains the JSON data files that are bundled with the package.
-These files are loaded by the core.db module to provide data access.
-"""
